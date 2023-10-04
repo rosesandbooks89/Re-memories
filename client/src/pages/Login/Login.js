fef2c4c2148c4182d7a3ba01bbf0b2f4541c79c4
rosesandbooks89 <rosesandbooks89@gmail.com>
2023-10-04 04:46:00 -0500

    seeing if typo fixes things

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,41 +1,32 @@
-import React, { useState } from "react";
-import { Form, Input, Button, Modal, Typography, message } from "antd";
-import { useMutation } from "@apollo/client";
-import { useNavigate } from "react-router-dom";
-import { LOGIN_USER } from "../../utils/mutations";
-
+import React, { useState } from 'react';
+import { Form, Input, Button, Modal, Typography, message } from 'antd';
+import { useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
+import { LOGIN_USER } from '../../utils/mutations';
 const { Title } = Typography;
-
 const Login = ({ visible, onClose, onSuccess }) => {
   const [loading, setLoading] = useState(false);
   const [login] = useMutation(LOGIN_USER);
   const navigate = useNavigate();
-
   const onFinish = async (values) => {
     setLoading(true);
+    console.log('Form values:', values);
     try {
       const { data } = await login({ variables: { ...values } });
-
+      console.log('Login Response:', data);
       if (data.login && data.login._id) {
+        console.log('Login Successful, User Data:', data.login);
         onSuccess(data.login.username);
-
-        message.success("Login successful");
+        message.success('Login successful');
         onClose();
-        navigate("/dashboard");
+        navigate('/dashboard');
       } else {
-        console.warn(
-          "Login data might be missing or structured differently:",
-          data
-        );
-        message.error("Invalid username or password");
+        console.warn('Login data might be missing or structured differently:', data);
+        message.error('Invalid username or password');
       }
     } catch (error) {
-      console.error(
-        "Authentication Error:",
-        error.message,
-        error.graphQLErrors
-      );
-      message.error("Invalid username or password");
+      console.error("Authentication Error:", error.message, error.graphQLErrors);
+      message.error('Invalid username or password');
     } finally {
       setLoading(false);
     }
@@ -52,14 +43,14 @@ const Login = ({ visible, onClose, onSuccess }) => {
         <Form.Item
           name="username"
           label="Username"
-          rules={[{ required: true, message: "Please enter your username" }]}
+          rules={[{ required: true, message: 'Please enter your username' }]}
         >
           <Input placeholder="Username" />
         </Form.Item>
         <Form.Item
           name="password"
           label="Password"
-          rules={[{ required: true, message: "Please enter your password" }]}
+          rules={[{ required: true, message: 'Please enter your password' }]}
         >
           <Input.Password placeholder="Password" />
         </Form.Item>
